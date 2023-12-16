b0ba15282e2f12abd08cff0d7db1067bca642acb
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-12-16 16:25:36 +0100

    Update password_reset.bat

--- Authentication/password_reset.bat
+++ Authentication/password_reset.bat
@@ -1,20 +1,20 @@
-# TODO test this out
-# TODO check Admin
+REM  # TODO test this out
+REM  # TODO check Admin
 
-# Batch file to reset the password
-# https://superuser.com/a/990040/1225570
-# https://stackoverflow.com/q/7879791/14346786
-# https://superuser.com/a/1067785/1225570
+REM # Batch file to reset the password
+REM # https://superuser.com/a/990040/1225570
+REM # https://stackoverflow.com/q/7879791/14346786
+REM # https://superuser.com/a/1067785/1225570
 
-# https://stackoverflow.com/a/7880263/14346786
-# https://stackoverflow.com/a/29158548/14346786
-## set /p asd="Hit enter to continue"
+REM # https://stackoverflow.com/a/7880263/14346786
+REM # https://stackoverflow.com/a/29158548/14346786
+REM ## set /p asd="Hit enter to continue"
 
-# read the username and put it in the env.var 'un'
-## (usable trough %un%)
+REM # read the username and put it in the env.var 'un'
+REM ## (usable trough %un%)
 set /p un="Enter the username:"
 echo "we will change the password for %un%"
 
-# TODO - make it so the password is read from another
-## Env.Variable
+REM # TODO - make it so the password is read from another
+REM ## Env.Variable
 net user %un% *
